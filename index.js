98fd7af36d0af71e701e02fac9ceb0b869b80031
billal-fahmid <billalcooom@gmail.com>
2023-09-08 23:30:43 +0600

    update myteams

--- index.js
+++ index.js
@@ -100,6 +100,23 @@ async function run() {
             res.send(result)
         })
 
+// 
+
+
+
+        app.patch('/update/myTeams/:id' , async(req,res) =>{
+            const id=req.params.id;
+            const newMyTeams = req.body
+            const filter={_id : new ObjectId(id)}
+            const updateDoc={
+                $set:{
+                    myTeams:newMyTeams
+                }
+            }
+            const result = await usersCollection.updateOne(filter , updateDoc);
+            res.send(result)
+        })
+
 
 
         
